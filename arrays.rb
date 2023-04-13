94173943569f5a6c5ea46ea921af250d320105c7
Victor Sanchez <vsanantone@gmail.com>
2023-04-12 19:03:14 -0500

    Add demonstration of index positions in document

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,29 @@
 
-vstrings = ["Apple", "Banana", "Court", "Daniel"]
+strings = ["Apple", "Banana", "Court", "Daniel"]
 numbers = [5, 6, 7, 8]
 decimals = [5.6, 6.7, 7.8, 8.9]
 truth_false = [true, false, false, true]
+
+#Removes the last element of the array and returns the value to the caller 
+puts strings.pop
+puts strings
+
+# Adds a specifiied element tothe end of an array and returns the new length of the array
+puts numbers.push(9)
+puts numbers
+
+# Removes the element at the zero index and shifts the values at consecutive indexes down, then returns the removed value
+puts decimals.shift
+puts decimals
+
+# Adds the specified element to the beggining of an array and returns the new length of the arra
+puts truth_false.unshift(false)
+puts truth_false
+
+puts strings[0] 
+puts numbers[1]
+puts decimals[2]
+
+if truth_false[3] == false
+    puts "Hello!"
+end
